1dc2d850f6a231c94534b3e012d036b0ec98dc1b
ashkr110 <122517019+ashkr110@users.noreply.github.com>
2024-10-11 03:07:46 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -16,6 +16,6 @@ terraform {
 provider "aws" {
   region                   = "ap-south-1"
   # shared_credentials_files = ["~/.aws/credentials"]
-  access_key = '${{ secrets.AWS_ACCESS_KEY_ID }}'
-  secret_key = '${{ secrets.AWS_SECRET_ACCESS_KEY }}'
+  # access_key = '${{ secrets.AWS_ACCESS_KEY_ID }}'
+  # secret_key = '${{ secrets.AWS_SECRET_ACCESS_KEY }}'
 }
